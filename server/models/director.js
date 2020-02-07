1ed5b6a72617f67c4f79c905625d1560dfffc349
Alan Santos Molina <alansantos@MacBook-Pro-de-Alan-2.local>
2020-02-06 21:45:38 -0600

    commit inicial para subir a heroku papapu

---
+++ server/models/director.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DirectorSchema = new Schema ({
+    name: {
+        type: String,
+        trim: true,
+    },
+    movies:[{
+    
+        type: Schema.Types.objectId,
+        ref: "Movie"
+
+    }]
+});
+const Director = mongoose.model("Director", DirectorSchema);
+module.exports = Director;
